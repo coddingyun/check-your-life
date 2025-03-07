eb63d970b479e20d24e2688e531fecbfb42bc23c
coddingyun <igy2840@gmail.com>
2025-03-07 19:56:32 +0900

    [SCRUM-17] Make ColorPickerDialog

---
+++ app/src/main/java/com/example/checkyourlife/ColorPickerDialog.kt
@@ -0,0 +1,69 @@
+package com.example.checkyourlife
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import androidx.hilt.navigation.compose.hiltViewModel
+
+@Composable
+fun ColorPickerDialog(
+    initialColor: Color,
+    colorPickerViewModel: ColorPickerViewModel = hiltViewModel(),
+    onDismiss: () -> Unit,
+    onConfirm: (Color) -> Unit
+) {
+    var selectedColor by remember { mutableStateOf(initialColor) }
+    var colorPickerState = colorPickerViewModel.colorPickerState.value
+
+    Dialog(onDismissRequest = { onDismiss() }) {
+        Card(
+            shape = RoundedCornerShape(12.dp),
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Column(
+                modifier = Modifier.padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = "색상을 선택하세요", fontSize = 18.sp, modifier = Modifier.padding(bottom = 16.dp))
+
+                ColorPicker(selectedColor = selectedColor) { newColor ->
+                    selectedColor = newColor
+                }
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Button(onClick = { onDismiss() }) {
+                        Text(text = "취소")
+                    }
+
+                    Button(onClick = { onConfirm(selectedColor) }) {
+                        Text(text = "확인")
+                    }
+                }
+            }
+        }
+    }
+}
