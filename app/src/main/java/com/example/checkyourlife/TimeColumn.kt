168945955d6856dc9ac4aefbfa1e352bebea5dc9
coddingyun <igy2840@gmail.com>
2025-03-12 06:33:21 +0900

    [SCRUM-35] TimeColumn Bug Fix

--- app/src/main/java/com/example/checkyourlife/TimeColumn.kt
+++ app/src/main/java/com/example/checkyourlife/TimeColumn.kt
@@ -1,25 +1,23 @@
 package com.example.checkyourlife
 
+import android.util.Log
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Divider
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.compose.ui.unit.times
 
 @Composable
 fun TimeColumn(hour: Int, scheduledActivities: List<Activity>?) {
     Box(Modifier.fillMaxSize()) {
         val activitiesForHour = scheduledActivities?.filter { it.startHour == hour }
+        val nextActivity = scheduledActivities?.filter { it.startHour > hour }?.minByOrNull { it.endTime + it.endMiniute.toFloat()/60 }
         val activitiesForMiddleHour = scheduledActivities?.filter {it.startHour < hour && ((it.endHour + it.endMiniute.toFloat()/60) > hour)}
 
         if (((activitiesForHour == null || activitiesForMiddleHour == null))
@@ -31,21 +29,22 @@ fun TimeColumn(hour: Int, scheduledActivities: List<Activity>?) {
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(60.dp), // 원하는 기본 높이 지정
-                //contentAlignment = Alignment.Center
             )
-//            {
-//                Text(text = "No Activity", color = Color.Gray, fontSize = 12.sp)
-//            }
         } else if (activitiesForHour != null) {
             if (activitiesForHour.isNotEmpty()) {
                 Divider()
                 activitiesForHour.forEach { activity ->
                     val (offset, height) = calculateActivityOffsetAndHeight(activity)
                     Column {
+                        val latestActivity = activitiesForMiddleHour.maxByOrNull { it.endTime + it.endMiniute / 60.0 }
+                        val emptyBoxHeightTop =
+                            if (latestActivity == null)
+                                    (activity.startMinute).mod(60).dp
+                            else activity.startMinute.dp - latestActivity.endMiniute.dp
+
                         Box(
-                            modifier = Modifier.fillMaxWidth().height((activity.startMinute).mod(60).dp)
+                            modifier = Modifier.fillMaxWidth().height(emptyBoxHeightTop)
                         )
-                        //{ Text("Upper Remainder", fontSize = 12.sp) }
                         ActivityBlock(
                             activity,
                             Modifier
@@ -53,10 +52,17 @@ fun TimeColumn(hour: Int, scheduledActivities: List<Activity>?) {
                                 .height(height)  // 활동의 길이 설정
                             //.offset(y = offset) // 시작 위치 조정
                         )
+
+                        //val nextActivity = activitiesForMiddleHour.maxByOrNull { it.endTime + it.endMiniute / 60.0 }
+
+                        val emptyBoxHeightBottom =
+                            if (nextActivity != null && (nextActivity.startHour == activity.endHour) && activity.endMiniute != 0)
+                                    (nextActivity.startMinute-activity.endMiniute).dp
+                            else (60-activity.endMiniute).mod(60).dp
+
                         Box(
-                            modifier = Modifier.fillMaxWidth().height((60-activity.endMiniute).mod(60).dp)
+                            modifier = Modifier.fillMaxWidth().height(emptyBoxHeightBottom)
                         )
-                        //{ Text("Lower Remainder", fontSize = 12.sp) }
                     }
 
                 }
@@ -73,5 +79,6 @@ fun calculateActivityOffsetAndHeight(activity: Activity): Pair<Dp, Dp> {
     val offset = (startMinutes / 60f) * 60.dp  // 분을 높이 비율로 변환
     val height = (durationMinutes / 60f) * 60.dp // 지속 시간도 높이로 변환
 
+    Log.i("height", "${activity.title}:${offset}")
     return Pair(offset, height)
 }
